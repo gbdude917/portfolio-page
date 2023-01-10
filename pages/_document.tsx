bb2f12aabd331fada97de6e58c7019d16c651d02
Gabriel <gabrielleong917@gmail.com>
2023-01-09 16:35:49 -0800

    added basic seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <title>Gabriel Leong - Portfolio Page</title>
+        <meta
+          name="description"
+          content="This is Gabriel's portfolio page. You can learn more about him here in his personal website."
+        />
+        <meta name="robots" content="index, archive" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
